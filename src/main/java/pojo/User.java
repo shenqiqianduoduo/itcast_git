fdb00363a74ab154cd08b6bd82563481e11ffc3e
shenduoduo <1092909385@qq.com>
2019-08-02 16:45:03 +0800

    firstcommit

---
+++ src/main/java/pojo/User.java
@@ -0,0 +1,15 @@
+package pojo;
+
+public class User {
+
+    private String name;
+    private Integer age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
