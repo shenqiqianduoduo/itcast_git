55018db1d96b30233f5b3aa40dfcd1d197026860
shenduoduo <1092909385@qq.com>
2019-08-02 17:11:05 +0800

    修改user

--- src/main/java/pojo/User.java
+++ src/main/java/pojo/User.java
@@ -2,14 +2,16 @@ package pojo;
 
 public class User {
 
-    private String name;
-    private Integer age;
+    private String name;   //用户名
+    private Integer age;   //年龄
 
     public void setName(String name) {
+
         this.name = name;
     }
 
     public void setAge(Integer age) {
+
         this.age = age;
     }
 }
